e2784e0f70160c1a5cbd03150b6b0aa7d8640c83
WangQiming <1358632259@qq.com>
2024-08-21 22:03:45 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/***
+ * @Author: WangQiming 1358632259@qq.com
+ * @Date: 2024-08-21 21:30:49
+ * @LastEditors: WangQiming 1358632259@qq.com
+ * @LastEditTime: 2024-08-21 22:02:56
+ * @FilePath: /cplusplus/src/main.cpp
+ * @Description:
+ * @Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
+
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+
+    return 0;
+}
+
+void foo()
+{
+}
