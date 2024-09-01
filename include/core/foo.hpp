7cab73a7070a40198e82b3f042c5e4f328013955
WangQiming <1358632259@qq.com>
2024-09-01 21:39:22 +0800

    vcpkg

---
+++ include/core/foo.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class Foo
+{
+public:
+    int value_a;
+    int value_b;
+    Foo(int a, int b) : value_a(a), value_b(b) {}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,58 @@
-/***
- * @Author: WangQiming 1358632259@qq.com
- * @Date: 2024-08-21 21:30:49
- * @LastEditors: WangQiming 1358632259@qq.com
- * @LastEditTime: 2024-08-21 22:02:56
- * @FilePath: /cplusplus/src/main.cpp
- * @Description:
- * @Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
- */
+#include "oatpp/network/Server.hpp"
+#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
+#include "oatpp/web/server/HttpConnectionHandler.hpp"
 
-#include <iostream>
-#include <vector>
+class HelloHandler : public oatpp::web::server::HttpRequestHandler
+{
+public:
+    std::shared_ptr<OutgoingResponse>
+    handle(const std::shared_ptr<IncomingRequest> &request) override
+    {
+        OATPP_LOGI("HelloHandler", "this is a request!");
+        auto i = request->getHeaders().get("User-Agent")->c_str();
+        OATPP_LOGI("HelloHandler", "User-Agent : %s", i);
+        return ResponseFactory::createResponse(Status::CODE_200, "Hello World");
+    }
+};
 
+void run()
+{
+
+    /* Create Router for HTTP requests routing */
+    auto router = oatpp::web::server::HttpRouter::createShared();
+
+    router->route("GET", "/hello", std::make_shared<HelloHandler>());
+    /* Create HTTP connection handler with router */
+    auto connectionHandler =
+        oatpp::web::server::HttpConnectionHandler::createShared(router);
+
+    /* Create TCP connection provider */
+    auto connectionProvider =
+        oatpp::network::tcp::server::ConnectionProvider::createShared(
+            {"localhost", 8000, oatpp::network::Address::IP_4});
+
+    /* Create server which takes provided TCP connections and passes them to HTTP
+     * connection handler */
+    oatpp::network::Server server(connectionProvider, connectionHandler);
+
+    /* Print info about server port */
+    OATPP_LOGI("MyApp", "Server running on port %s",
+               connectionProvider->getProperty("port").getData());
+    /* Run server */
+    server.run();
+}
 
 int main()
 {
 
-    return 0;
-}
+    /* Init oatpp Environment */
+    oatpp::base::Environment::init();
 
-void foo()
-{
+    /* Run App */
+    run();
+
+    /* Destroy oatpp Environment */
+    oatpp::base::Environment::destroy();
+
+    return 0;
 }
